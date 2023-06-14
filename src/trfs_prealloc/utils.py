e921da2fa9808631fe8b172189f72e48b175ec49
Félix Marty <9808326+fxmarty@users.noreply.github.com>
2023-06-14 23:03:38 +0900

    add utils

---
+++ src/trfs_prealloc/utils.py
@@ -0,0 +1,28 @@
+import functools
+import copy
+
+def recurse_getattr(obj, attr: str):
+    def _getattr(obj, attr):
+        return getattr(obj, attr)
+
+    return functools.reduce(_getattr, [obj] + attr.split("."))
+
+def recurse_hasattr(obj, attr):
+    try: left, right = attr.split('.', 1)
+    except: return hasattr(obj, attr)
+    return recurse_hasattr(getattr(obj, left), right)
+
+def recurse_setattr(module, name, value):
+    if "." not in name:
+        setattr(module, name, value)
+    else:
+        name, rest = name.split(".", 1)
+        recurse_setattr(getattr(module, name), rest, value)
+
+def recurse_delattr(obj, attr: str):
+    if "." not in attr:
+        delattr(obj, attr)
+    else:
+        root = ".".join(attr.split(".")[:-1])
+        end = attr.split(".")[-1]
+        delattr(recurse_getattr(obj, root), end)
